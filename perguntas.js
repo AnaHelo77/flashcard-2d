ba8b1d88674a6255ed14a2860f402e951bf4fa06
AnaHelo77 <ana.bobato.palhano@escola.pr.gov.br>
2025-06-24 11:51:59 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'biologia',
+'você gosta de biologia?',
+    'sim'
+)
+
+criaCartao(
+    'programação',
+    'gosta de programação?',
+    'mais o menos'
+)
+
+
+criaCartao(
+     'matemática',
+    'qual o número PI?',
+    '3,14'
+)
+
+criaCartao(
+    'inglês',
+'como é dia do pagamento em inglês?',
+    'pay day'
+)
+
+criaCartao(
+    'geografia',
+    ' o que é planície?',
+    'área geográfica extensa e relativamente plana...'
+)
+
+criaCartao(
+     'matemática',
+    'gosta de matemática?',
+    'não'
+)
